a2eadf506907f9b1e141b06e4066125fc9433638
Savitha P <129480387+savitha3306@users.noreply.github.com>
2023-09-14 13:43:43 +0530

    Update file.py

--- file.py
+++ file.py
@@ -21,23 +21,55 @@ def bert_recommend(target_book_title):
     lst=[]
     for idx in recommended_unique_books_indices:
         lst.append(dic_book[idx])
-    return lst
+    return lst 
+
+# Define the spacing width between images 
+space_width = 20  # in pixels
 
 st.title("Book Recommendation For You")
 
 books=st.selectbox("Please enter a book",sel_books)
-try:
-    st.image(d[books])
-except:
-    st.write("Image not Available")
+
+images = d[books]
+num_images = len(images)
+    
+# Calculate the number of columns in each row to create in the layout
+num_columns = 5
+columns = [st.columns(num_columns) for _ in range((num_images + num_columns - 1) // num_columns)]
+    
+for i, col in enumerate(columns):
+    for j in range(num_columns):
+        index = i * num_columns + j
+        if index < num_images:
+            try:
+                col[j].image(images[index], use_column_width=True)
+            except:
+                col[j].write("Image not Available")
+        else:
+            col[j].write(" " * space_width)  # Create spacing
+
 if st.button("Submit"):
     recmd=bert_recommend(books)
-    for i in recmd:
 
-        st.header(i)
-        try:
-            st.image(d[i])
-        except:
-            st.write("Image not Available")
+    for book in recmd:
+        st.header(book)
+    
+        images = d[book]
+        num_images = len(images)
+    
+        # Calculate the number of columns in each row to create in the layout
+        num_columns = 4  
+        columns = [st.columns(num_columns) for _ in range((num_images + num_columns - 1) // num_columns)]
+    
+        for i, col in enumerate(columns):
+            for j in range(num_columns):
+                index = i * num_columns + j
+                if index < num_images:
+                    try:
+                        col[j].image(images[index], use_column_width=True)
+                    except:
+                        col[j].write("Image not Available")
+                else:
+                    col[j].write(" " * space_width)  # Create spacing
 
     st.write("Book selected succesfully")
